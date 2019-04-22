f683db37bc379de48438f0f922d7f32cefc41888
01Jonathan10 <jonathansuper1@gmail.com>
2019-04-22 10:11:17 -0300

    DNS search field

--- src/ApplicationLayer/main.lua
+++ src/ApplicationLayer/main.lua
@@ -5,9 +5,9 @@ DNS_table = {}
 sorted_ips = {}
 DNS_log = {}
 curr = ''
+typing_check = ''
 
 function love.load(arg)
-	print(load_table)
 	DNS_table = load_table()
 
 	log_timer = 0
@@ -20,22 +20,23 @@ function love.update(dt)
 	end
 end
 
+function love.textinput(text)
+	typing_check = typing_check..text
+end
+
 function love.keypressed(key)
 	if key == 'escape' then
 		love.event.push('quit')
 	end
 	
-	if key == 'q' then
-		new_request("192.168.10.1", DNS_table, DNS_log)
+	if key == 'return' and typing_check then
+		new_request(typing_check, DNS_table, DNS_log)
+		typing_check = ''
+		return
 	end
-	if key == 'w' then
-		new_request("192.168.10.5", DNS_table, DNS_log)
-	end
-	if key == 'e' then
-		new_request("pc_igor", DNS_table, DNS_log)
-	end
-	if key == 'r' then
-		new_request("pc_batata", DNS_table, DNS_log)
+	
+	if key == "backspace" and typing_check then
+		typing_check = typing_check:sub(1, #typing_check - 1)
 	end
 end
 
@@ -44,6 +45,9 @@ function love.draw()
 	
 	draw_table()
 	draw_log()
+	
+	love.graphics.print("Search: " .. typing_check,20, 500)
+	love.graphics.print("Press Enter to search", 20, 540)
 end
 
 function draw_table()
