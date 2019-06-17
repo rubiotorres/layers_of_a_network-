82fb088764b86c3e37741a8a65c46536916ec6c1
Igor Oliveira <m.igoroliveira@gmail.com>
2019-06-17 13:13:30 -0300

    Initial NetworkLayer

---
+++ src/NetworkLayer/client.js
@@ -0,0 +1,15 @@
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(1337, '127.0.0.1', function() {
+	console.log('Connected');
+});
+
+client.on('data', function(data) {
+	console.log('Recebido: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Encerrada');
+});
